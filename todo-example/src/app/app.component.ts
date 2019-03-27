dc55eea590b995f29d36c5986317327e3735d32d
Max Gusenbauer <max.gusenb@gmail.com>
2019-03-27 15:00:03 +0100

    Finished all Methods except edit

--- todo-example/src/app/app.component.ts
+++ todo-example/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-interface IPerson { name: string; }
+import { OnInit } from '@angular/core';
+import {NgModule} from '@angular/core';
+interface IPerson { name: String };
+interface IToDo { id: number, description: String, assignedTo: String, done: boolean};
 
 @Component({
   selector: 'app-root',
@@ -9,13 +12,65 @@ interface IPerson { name: string; }
 })
 
 export class AppComponent {
+  public todos: IToDo[];
   public people: IPerson[];
+  public filter: number = 0;
+  public newDescription: String;
+  public newPersonName: String;
+  public personName: String = "";
   
   constructor(private httpClient: HttpClient) { 
-    this.loadPeople;
+    
+  }
+
+  ngOnInit() {
+    this.loadTodos();
+    this.loadPeople();
+  }
+
+  async loadTodos(){
+    this.todos = await this.httpClient
+      .get<IToDo[]>('http://localhost:8080/api/todos')
+      .toPromise();
+    if (this.filter == 2){
+      this.todos = this.todos.filter(this.checkUndone);
+    }
+    if (this.filter == 1){
+      this.todos = this.todos.filter(this.checkDone);
+    }
+    if (this.personName != ""){
+      this.todos = this.todos.filter(todo => todo.assignedTo == this.personName);
+    }
   }
 
   async loadPeople(){
-    this.people = await this.httpClient.get<IPerson[]>('http://localhost:8080/api/people').toPromise();
+    this.people = await this.httpClient
+      .get<IPerson[]>('http://localhost:8080/api/people')
+      .toPromise();
+  }
+
+  async createToDo(){
+    await this.httpClient.post('http://localhost:8080/api/todos', {
+      "description": this.newDescription,
+      "assignedTo": this.newPersonName
+    }).toPromise(); 
+    this.loadTodos();
+  }
+
+  async deleteToDo(id: number){
+    await this.httpClient.delete(`http://localhost:8080/api/todos/${id}`).toPromise();
+    this.loadTodos();
+  }
+
+  public checkUndone(todo: IToDo){
+    return !todo.done;
+  }
+
+  public checkDone(todo: IToDo){
+    return todo.done;
+  }
+
+  printTodos(){
+    console.log(this.todos);
   }
 }
--- todo-example/src/app/app.module.ts
+++ todo-example/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatSelectModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 
@@ -19,7 +23,12 @@ import { AppComponent } from './app.component';
     MatButtonModule,
     MatCheckboxModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatButtonToggleModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
